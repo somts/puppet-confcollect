f3dd61e1035aafedad99430b61a60b30715f63ad
Jon C. Meyer <jmeyer@ucsd.edu>
2018-07-07 08:48:41 -0700

    HSN-861 oooops

--- files/collectqflex.py
+++ files/collectqflex.py
@@ -34,7 +34,7 @@ def get_qflex_data(filecmdlist, nm_kwargs, logger, enable=False, ending=None):
                 logger.debug('Sending enable command to %s', hostinfo)
                 net_connect.enable()
 
-            for fname, cmd in filecmdlist():
+            for fname, cmd in filecmdlist:
                 logger.debug('Sending command "%s" to %s', cmd, hostinfo)
                 output = net_connect.send_command(cmd)
 
