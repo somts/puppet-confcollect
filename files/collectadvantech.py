fae89563b1590a546bb394028300850a1e92ed94
Jon C. Meyer <jmeyer@ucsd.edu>
2019-02-20 05:18:03 -0800

    tighten up exception logic for collectadvantech.py

--- files/collectadvantech.py
+++ files/collectadvantech.py
@@ -24,6 +24,7 @@ def cfgworker(host, loglevel,
     url2 = "http://%s:%i/cgi-bin/index.cgi?func=doexport" % (host, port)
 
     url = url1
+    response = None
 
     logger = setup_logger('collectadvantech_%s' % host,
                           path.join(log_dir, 'collectadvantech.%s.log' % host),
@@ -34,36 +35,35 @@ def cfgworker(host, loglevel,
     if username is None and password is None:
         # Out of the box, there seems to be no password Advantechs
         try:
-            response = requests.get(url1)
+            response = requests.get(url1, timeout=(18.5, 90.5))
 
             if response.status_code == 404:
                 logger.debug('Done talking to %s.', url)
                 logger.debug('Attempting to talk to %s ...', url2)
-                response = requests.get(url2)
+                response = requests.get(url2, timeout=(18.5, 90.5))
                 url = url2
 
-        except requests.exceptions.ConnectionError as err:
+        except (requests.exceptions.ConnectTimeout,
+                requests.exceptions.ConnectionError) as err:
             logger.info('Error with %s: "%s".', host, err)
     else:
         logger.warn('username/password unsupported for collectadvnatech')
 
     logger.debug('Done talking to %s.', url)
 
-    if 'response' in locals():
+    if response is not None:
         # Write data to disk
         fname = path.join(destination_dir, host + '.conf')
         logger.debug('Opening %s for writing conf data...', fname)
-        myfile = open(fname, 'w')
+
         # We must write as UTF-8 to avoid errors like:
         # UnicodeEncodeError: 'ascii' codec can't encode character u'\x84'
         # in position 2594: ordinal not in range(128)
         # ...from the Healy CLAB1/CLAB2 devices.  Not sure why.
-        myfile.write(response.text.encode('utf-8'))
-        myfile.close()
+        with open(fname, 'w') as myfile:
+            myfile.write(response.text.encode('utf-8'))
 
         logger.debug('conf data saved to %s.', fname)
 
     logger.info('END %s', host)
-
-
 #pylint: enable=too-many-arguments
