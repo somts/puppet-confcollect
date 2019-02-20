64d5812c5b186ac7836760f2671b78efc9ed4f71
Jon C. Meyer <jmeyer@ucsd.edu>
2019-02-20 04:46:56 -0800

    Catch requests.exceptions.ConnectionError exceptions

--- files/collectadvantech.py
+++ files/collectadvantech.py
@@ -33,28 +33,37 @@ def cfgworker(host, loglevel,
 
     if username is None and password is None:
         # Out of the box, there seems to be no password Advantechs
-        response = requests.get(url1)
-        if response.status_code == 404:
-            logger.debug('Done talking to %s.', url)
-            logger.debug('Attempting to talk to %s ...', url2)
-            response = requests.get(url2)
-            url = url2
+        try:
+            response = requests.get(url1)
+
+            if response.status_code == 404:
+                logger.debug('Done talking to %s.', url)
+                logger.debug('Attempting to talk to %s ...', url2)
+                response = requests.get(url2)
+                url = url2
+
+        except requests.exceptions.ConnectionError as err:
+            logger.info('Error with %s: "%s".', host, err)
     else:
         logger.warn('username/password unsupported for collectadvnatech')
 
     logger.debug('Done talking to %s.', url)
 
-    # Write data to disk
-    fname = path.join(destination_dir, host + '.conf')
-    logger.debug('Opening %s for writing conf data...', fname)
-    myfile = open(fname, 'w')
-    # We must write as UTF-8 to avoide errors like:
-    # UnicodeEncodeError: 'ascii' codec can't encode character u'\x84'
-    # in position 2594: ordinal not in range(128)
-    # ...from the Healy CLAB1/CLAB2 devices.  Not sure why.
-    myfile.write(response.text.encode('utf-8'))
-    myfile.close()
+    if 'response' in locals():
+        # Write data to disk
+        fname = path.join(destination_dir, host + '.conf')
+        logger.debug('Opening %s for writing conf data...', fname)
+        myfile = open(fname, 'w')
+        # We must write as UTF-8 to avoid errors like:
+        # UnicodeEncodeError: 'ascii' codec can't encode character u'\x84'
+        # in position 2594: ordinal not in range(128)
+        # ...from the Healy CLAB1/CLAB2 devices.  Not sure why.
+        myfile.write(response.text.encode('utf-8'))
+        myfile.close()
 
-    logger.debug('conf data saved to %s.', fname)
+        logger.debug('conf data saved to %s.', fname)
+
     logger.info('END %s', host)
+
+
 #pylint: enable=too-many-arguments
